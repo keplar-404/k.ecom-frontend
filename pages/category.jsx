cc1da893b19c5c8e1e3cff01f987ce29891524ba
keplar-404 <shehabhossen76.services@gmail.com>
2023-03-08 01:47:29 +0600

    bug fixes for rating

--- pages/category.jsx
+++ pages/category.jsx
@@ -21,7 +21,7 @@ function category() {
   const [selectedRatings, setSelectedRatings] = useState([]);
   // setting default sort filter
   const [_sort, setSort] = useState("");
-  const [load, setLoad] = useState(true)
+  const [load, setLoad] = useState(true);
 
   // getting all products from backend
   useEffect(() => {
@@ -30,16 +30,9 @@ function category() {
       .then((data) => {
         const allproducts = data.data.data;
 
-
-
-
-
         setProducts(allproducts);
       });
   }, []);
-
-
-
 
   if (products == null) {
     return (
@@ -58,66 +51,65 @@ function category() {
       </>
     );
   } else {
-
-    
-    
-    
-    
     // getting item by search
     let itemGetBySearch;
     if (products !== null) {
       // console.log(products)
       itemGetBySearch = products.filter((e) =>
-      search === "" ? products : e.name.toLowerCase().includes(search)
+        search === "" ? products : e.name.toLowerCase().includes(search)
       );
-  } else {
-    return;
-  }
-  
-  // By calling this function will give you the final reasult based on your search, category, rating, price low to high or high to low filter
-  const t = () => {
-    if (selectedRatings.length) {
-      const rafR = getItemByCategory.filter((p) =>
-      selectedRatings.includes(Math.floor(p.rating))
-      );
-      if (_sort == "lowtohigh") {
-        const sortResult = rafR.sort((a, b) => a.price - b.price);
-        // console.log(sortResult)
-        finalResult = sortResult;
-      } else if (_sort == "hightolow") {
-        const sortResult = rafR.sort((a, b) => b.price - a.price);
-        finalResult = sortResult;
-      } else {
-        finalResult = rafR;
-      }
-      // console.log(finalResult);
     } else {
-      const rafR = getItemByCategory;
-      if (_sort == "lowtohigh") {
-        const sortResult = rafR.sort((a, b) => a.price - b.price);
-        // console.log(sortResult)
-        finalResult = sortResult;
-      } else if (_sort == "hightolow") {
-        const sortResult = rafR.sort((a, b) => b.price - a.price);
-        finalResult = sortResult;
-      } else {
-        finalResult = rafR;
-      }
-      // console.log(finalResult);
+      return;
     }
-  };
-  
-  // getting item by category based on search
-  if (getCategory === "all") {
-    getItemByCategory = itemGetBySearch;
-    t();
-  } else {
-    getItemByCategory = itemGetBySearch.filter(
-      (e) => e.category === getCategory
+
+    // By calling this function will give you the final reasult based on your search, category, rating, price low to high or high to low filter
+    const t = () => {
+      if (selectedRatings.length) {
+        const rafR = getItemByCategory.filter((p) =>
+          selectedRatings.includes(
+            Math.floor(
+              p.rating.reduce((acc, value) => acc + value, 0) / p.rating.length
+            )
+          )
+        );
+        if (_sort == "lowtohigh") {
+          const sortResult = rafR.sort((a, b) => a.price - b.price);
+          // console.log(sortResult)
+          finalResult = sortResult;
+        } else if (_sort == "hightolow") {
+          const sortResult = rafR.sort((a, b) => b.price - a.price);
+          finalResult = sortResult;
+        } else {
+          finalResult = rafR;
+        }
+        // console.log(finalResult);
+      } else {
+        const rafR = getItemByCategory;
+        if (_sort == "lowtohigh") {
+          const sortResult = rafR.sort((a, b) => a.price - b.price);
+          // console.log(sortResult)
+          finalResult = sortResult;
+        } else if (_sort == "hightolow") {
+          const sortResult = rafR.sort((a, b) => b.price - a.price);
+          finalResult = sortResult;
+        } else {
+          finalResult = rafR;
+        }
+        // console.log(finalResult);
+      }
+    };
+
+    // getting item by category based on search
+    if (getCategory === "all") {
+      getItemByCategory = itemGetBySearch;
+      t();
+    } else {
+      getItemByCategory = itemGetBySearch.filter(
+        (e) => e.category === getCategory
       );
       t();
     }
-    
+
     // rating handler
     let rating = [];
     const handleRating = (e) => {
@@ -135,7 +127,7 @@ function category() {
         // console.log(rating)
       }
     };
-    
+
     // sort handler
     const handleSort = (e) => {
       let value = e.target.value;
@@ -150,37 +142,35 @@ function category() {
       }
     };
     // console.log(sort)
-    
-    
-    
+
     return (
       <>
-      <div className="container w-full h-full px-14 bg-slate-50">
-        <div>
-          <h1 className="text-2xl text-center pt-9">All Products</h1>
-        </div>
-        <div className="flex flex-row pt-12">
-          <div className="w-1/5">
-            {/* left all filter */}
-            <Left
-              getCategory={setGetCategory}
-              setSearch={setSearch}
-              handleRating={handleRating}
-              handleSort={handleSort}
-              />
+        <div className="container w-full h-full px-14 bg-slate-50">
+          <div>
+            <h1 className="text-2xl text-center pt-9">All Products</h1>
           </div>
-          <div className="w-4/5">
-            <Right
-              data={finalResult}
-              itemsPerPage={8}
-              addToCartHandler={addToCartHandler}
+          <div className="flex flex-row pt-12">
+            <div className="w-1/5">
+              {/* left all filter */}
+              <Left
+                getCategory={setGetCategory}
+                setSearch={setSearch}
+                handleRating={handleRating}
+                handleSort={handleSort}
               />
+            </div>
+            <div className="w-4/5">
+              <Right
+                data={finalResult}
+                itemsPerPage={8}
+                addToCartHandler={addToCartHandler}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-}
+      </>
+    );
+  }
 }
 
 export default category;
